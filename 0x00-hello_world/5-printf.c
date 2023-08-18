8489d745b0150b4fe39524acd8d041b0f31ca20a
Mshaheen0 <mohammed999.ws@gmail.com>
2023-08-18 23:16:28 +0000

    printf file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: program that print printf function
+ * Return: always 0 (sucsess)
+ */
+int main(void)
+{
+	pritnf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
+
