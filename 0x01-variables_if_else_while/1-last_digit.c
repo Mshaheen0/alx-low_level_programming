a45fb321f01773d73eb369e7e0d0d2f8ea17b96c
Mshaheen0 <mohammed999.ws@gmail.com>
2023-08-24 20:55:18 +0000

    0. Positive anything is better than negative nothing e1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entery point
+ * Descrition: This program will assign a random number to the variable n
+ * each time it is executed
+ * Return: always 0 (successful)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	int s = n % 10;
+
+	printf("Last digit of %d is %d ", n, s);
+		if (s > 5)
+			printf("and is greater than 5");
+		else
+			if (s = 0)
+			printf("and is 0");
+			else
+			printf("and is less than 6 and not 0");
+	printf("\n");
+	return (0);
+}
