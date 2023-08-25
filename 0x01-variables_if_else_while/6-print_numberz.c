744a2fd09240a2a9be19a08b7e01cbc18eb6aaef
Mshaheen0 <mohammed999.ws@gmail.com>
2023-08-25 09:12:38 +0000

    6. Numberz e1

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entery point
+ * Descrition: a program that prints all single digit numbers of
+ * base 10 starting from 0, followed by a new line.
+ * Return: always 0 (successful)
+ */
+int main(void)
+{
+	int x = (48);
+
+	while (x < (58))
+	{
+		putchar(x);
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
