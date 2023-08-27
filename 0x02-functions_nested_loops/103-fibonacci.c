c4d70a790919ad72b670933e9cc5a0fcfaa7489d
Mshaheen0 <mohammed999.ws@gmail.com>
2023-08-27 00:48:27 -0700

    dgd

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	float total_sum;
+
+	while (1)
+	{
+		sum = fib1 + fib2;
+
+		if (sum > 4000000)
+			break;
+		
+	}
+}
