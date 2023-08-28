70e4e440aab2898bf34cfed8497e185683a33b64
SHVHEEN <84326756+Mshaheen0@users.noreply.github.com>
2023-08-28 23:13:52 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,5 +16,15 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[0]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif
