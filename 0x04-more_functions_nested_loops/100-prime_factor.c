20ce658103ae5d3ce07defd6ea06a9f9060b3b13
Mshaheen0 <mohammed999.ws@gmail.com>
2023-08-29 19:58:49 +0000

    programm that calculate prime numbers edit 0

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	long int n = 612852475143;
+	int i;
+
+	while (n % 2 == 0)
+	{
+		printf("%d ", 2);
+		n = n / 2;
+	}
+	for (i = 3; i*i <= n; i = i+2)
+	{
+		while (n % i == 0)
+		{
+			printf("%d ", i);
+			n = n/i;
+		}
+	}
+	if (n > 2)
+		printf ("%ld ", n);
+	printf("\n");
+	return 0;
+}
+ 
