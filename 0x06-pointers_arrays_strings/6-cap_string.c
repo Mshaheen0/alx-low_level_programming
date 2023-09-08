023dc91eca201bc3682daa4280cd6c94770b8c5c
Mshaheen0 <mohammed999.ws@gmail.com>
2023-09-08 13:10:56 +0000

    5-string_toupper.c modded

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,7 +16,7 @@ char *cap_string(char *str)
 				(str[i - 1] == '!') || (str[i - 1] == '?') ||
 				(str[i - 1] == '"') || (str[i - 1] == '(') ||
 				(str[i - 1] == ')') || (str[i - 1] == '{') ||
-				(str[i - 1] == '}'))
+				(str[i - 1] == '}') || i - 1 < 0)
 		{
 			if (str[i] >= 'a' && str[i] <= 'z')
 				str[i] = str[i] - 32;
