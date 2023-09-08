512d44e23d225c8aa66fbd3772855a727ad82088
Mshaheen0 <mohammed999.ws@gmail.com>
2023-09-08 06:38:26 +0000

    mod1 3-strcmp.c file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,7 +7,18 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int cmp = *s1 - *s2;
+	int equal = 0;
 
-	return (cmp);
+	while (*s1)
+	{
+		if (*s1 != *s2)
+		{
+			equal = ((int) *s1 - 48) - ((int) *s2 - 48);
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return (equal);
 }
+
