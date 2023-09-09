1198d9d01f198fa64f7737b9c5d6857d8323d9a9
Mshaheen0 <mohammed999.ws@gmail.com>
2023-09-09 14:05:07 +0000

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strstr - a function that gets the length of a prefix substring.
+ * @haystack: the string.
+ * @needle: the word.
+ * Return: Returns a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 1; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] == needle[j])
+					continue;
+				else
+					break;
+			}
+		}
+		if (j == 5)
+			return (haystack + i);
+	}
+	return (NULL);
+}
