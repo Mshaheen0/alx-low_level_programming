44f1c67681cb329de064d198eec2b77e21b07652
Mshaheen0 <mohammed999.ws@gmail.com>
2023-09-12 10:15:52 +0000

    1-args.c added

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point.
+ * @argc: the number of arguments.
+ * @argv: the array of argument's string.
+ * Return: always 0 .
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	(void)argc;
+	(void)argv; 
+	return (0);
+}
