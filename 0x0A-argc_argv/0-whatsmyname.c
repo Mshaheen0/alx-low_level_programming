3b86d5ca4996d9012325164397613c00f5bad7bb
Mshaheen0 <mohammed999.ws@gmail.com>
2023-09-12 10:12:24 +0000

    0-whatsmyname.c added

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - entry point.
+ * @argc: the number of arguments.
+ * @argv: the array of argument's string.
+ * Return: always 0 .
+ */
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
